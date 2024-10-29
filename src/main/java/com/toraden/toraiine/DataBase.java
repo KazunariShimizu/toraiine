90e7a6d75002cadf23774ea98cc7f815177770d1
Kazunari Shimizu <shimizu@tora-v.net>
2024-10-30 00:55:32 +0900

    DB timeout correction

--- src/main/java/com/toraden/toraiine/DataBase.java
+++ src/main/java/com/toraden/toraiine/DataBase.java
@@ -7,9 +7,22 @@ import java.sql.SQLException;
 public class DataBase {
     private static DataBase instance;
     private static Connection con;
+    private final String dbName;
+    private final String dbUrl;
+    private final String dbUser;
+    private final String dbPassword;
 
     // シングルトン用コンストラクタ
     private DataBase(String dbName, String dbUrl, String dbUser, String dbPassword) {
+        this.dbName = dbName;
+        this.dbUrl = dbUrl;
+        this.dbUser = dbUser;
+        this.dbPassword = dbPassword;
+        connect();
+    }
+
+    // データベースに接続するメソッド
+    private void connect() {
         try {
             // Load JDBC Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -21,6 +34,7 @@ public class DataBase {
         }
     }
 
+    // インスタンスを取得するメソッド
     public static DataBase getInstance(String dbName, String dbUrl, String dbUser, String dbPassword) {
         if (instance == null) {
             instance = new DataBase(dbName, dbUrl, dbUser, dbPassword);
@@ -28,7 +42,16 @@ public class DataBase {
         return instance;
     }
 
+    // 有効な接続を返すメソッド
     public Connection getConnection() {
+        try {
+            // 接続が閉じられている場合は再接続
+            if (con == null || con.isClosed()) {
+                connect();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return con;
     }
 }
