25a24f816e92e2c7e5df66c2643ca8c9f0d3fe8c
Junki Kizuka <k.falconws@gmail.com>
2024-11-03 16:28:56 +0900

    Fix DB CommunicationsException WARN

--- src/main/java/com/toraden/toraiine/DataBase.java
+++ src/main/java/com/toraden/toraiine/DataBase.java
@@ -6,38 +6,45 @@ import java.sql.SQLException;
 
 public class DataBase {
     private static DataBase instance;
-    private static Connection con;
-    private final String dbName;
-    private final String dbUrl;
-    private final String dbUser;
-    private final String dbPassword;
+    private static Connection connection;
+    private final String name;
+    private final String url;
+    private final String user;
+    private final String password;
 
     // シングルトン用コンストラクタ
-    private DataBase(String dbName, String dbUrl, String dbUser, String dbPassword) {
-        this.dbName = dbName;
-        this.dbUrl = dbUrl;
-        this.dbUser = dbUser;
-        this.dbPassword = dbPassword;
+    // 1回しか呼ばれない
+    private DataBase(String name, String url, String user, String password) {
+        this.name = name;
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        try {
+            // 1回だけDriverを初期化（forName）すれば良い
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         this.connect();
     }
 
-    // データベースに接続するメソッド
+    // データベースに接続する
     private void connect() {
         try {
-            // Load JDBC Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
             // データベースに接続しconnectionオブジェクトを取得する
-            con = DriverManager.getConnection("jdbc:mysql://%s/%s".formatted(this.dbUrl, this.dbName), this.dbUser, this.dbPassword);
-        } catch (ClassNotFoundException | SQLException e) {
+            connection = DriverManager.getConnection("jdbc:mysql://%s/%s?".formatted(this.url, this.name), this.user, this.password);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     // インスタンスを取得するメソッド
-    public static DataBase getInstance(String dbName, String dbUrl, String dbUser, String dbPassword) {
+    public static DataBase getInstance(String name, String url, String user, String password) {
         if (instance == null) {
-            instance = new DataBase(dbName, dbUrl, dbUser, dbPassword);
+            // 最初の1回だけここが実行される
+            // 最初の1回以外はgetInstance(null, null,...)等で呼ばれてもここを通らないので引数が無視される
+            // 最初の1回でthis.nameやthis.urlに正しい値を保存しているのでthis.connect()は問題なく実行出来るロジック
+            instance = new DataBase(name, url, user, password);
         }
         return instance;
     }
@@ -45,13 +52,13 @@ public class DataBase {
     // 有効な接続を返すメソッド
     public Connection getConnection() {
         try {
-            // 接続が閉じられている場合は再接続
-            if (con == null || con.isClosed()) {
-                this.connect();
+            // 接続が閉じられているか簡易なQueryを投げて例外の発生有無で確認する
+            if (connection != null) {
+                connection.prepareStatement("SELECT 1").executeQuery();
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            this.connect();
         }
-        return con;
+        return connection;
     }
 }
